4e48d03acd58061fe5462d5d86796ee294d6aa34
Rutger <rutgerjmckenna@gmail.com>
2019-09-14 15:03:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (block) {
+    if (block > 42) {
+      return block - 42;
+    } else {
+      return 42 - block;
+    }
+  }
+  
+function distanceFromHqInFeet (block) {
+    return distanceFromHqInBlocks(block) * 264;
+  }
+
+function distanceTravelledInFeet (start, end) {
+    if (start < end) {
+        return (end - start) * 264;
+    } else {
+        return (start - end) * 264;
+    }
+}
+
+function calculatesFarePrice (start, end) {
+    const travel = distanceTravelledInFeet(start, end);
+
+    if (travel <= 400) {
+        return 0;
+    } else if (travel > 400 && travel < 2000) {
+        return (travel - 400) * 0.02;
+    } else if (travel > 2000 && travel < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
